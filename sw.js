7963202cc9d0e69612c31cf45261e1712025c685
atuldhuvad-cmd <atuldhuvad@gmail.com>
2026-08-24 15:55:22 -0400

    Release Mobile Tally PWA v1.4.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mobile-tally-shell-v6';
+const CACHE_NAME = 'mobile-tally-shell-v7';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const APP_SHELL = new URL(BASE_PATH, self.location.origin).toString();
 const SHELL = [
