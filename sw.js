28dc0bec91773e9ee0080252c802614244c22121
atuldhuvad-cmd <atuldhuvad@gmail.com>
2026-08-25 13:31:52 -0400

    Release Mobile Tally PWA v1.5.3 - 4-Category More Hub layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mobile-tally-shell-v15';
+const CACHE_NAME = 'mobile-tally-shell-v16';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const APP_SHELL = new URL(BASE_PATH, self.location.origin).toString();
 const SHELL = [
