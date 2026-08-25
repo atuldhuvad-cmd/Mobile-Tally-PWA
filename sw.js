9bd57b74ae2bdf0e0189a8403370e82eeb8c8cd6
atuldhuvad-cmd <atuldhuvad@gmail.com>
2026-08-25 13:54:57 -0400

    Release Mobile Tally PWA v1.5.4 - official Tally Ledger & Rupee branding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mobile-tally-shell-v16';
+const CACHE_NAME = 'mobile-tally-shell-v17';
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const APP_SHELL = new URL(BASE_PATH, self.location.origin).toString();
 const SHELL = [
